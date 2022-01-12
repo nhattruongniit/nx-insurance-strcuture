e8fe3d325810165df7cc47b866b72056a972b734
Tony Nguyen <tony.nguyen@usonialabs.com>
2022-01-12 20:19:49 +0700

    initial commit

---
+++ apps/aman/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="aman" />
+      <div />
+    </>
+  );
+}
+
+export default App;
